7ddc7e9cb745041e27c6f2e07618b3ea73e29d97
Solee Lee <carolinesoleelee@gmail.com>
2018-09-21 02:54:30 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,9 +1,9 @@
 def using_times
-	Wingardium_Leviosa = 0
+	looping_string = 0
   loop do
-    lWingardium_Leviosa += 1
-    puts "Iteration #{Wingardium_Leviosa} of the loop"
-  if Wingardium_Leviosa >= 7
+    looping_string += 1
+    puts "Iteration #{looping_string} of the loop"
+  if looping_string >= 7
     break
 end
 end
