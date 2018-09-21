514309e3f01d4d9dc0fd8bb5c276bc6cae822f58
Solee Lee <carolinesoleelee@gmail.com>
2018-09-21 03:03:43 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -2,7 +2,7 @@ def using_times
 	looping_string = 0
   loop do
     looping_string += 1
-    puts "Iteration #{looping_string} of the loop"
+    puts "#{looping_string}"
   if looping_string >= 7
     break
 end
